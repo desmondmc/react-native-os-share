8c932560cde168b395ffe05b6e8a18e42caa446e
Desmond McNamee <desmondmcnamee@gmail.com>
2020-01-08 11:46:23 +0100

    Initial android implementation

--- android/src/main/java/com/reactlibrary/OsShareModule.java
+++ android/src/main/java/com/reactlibrary/OsShareModule.java
@@ -1,9 +1,15 @@
 package com.reactlibrary;
 
+import android.content.Intent;
+import android.net.Uri;
+
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableMap;
+
+import java.io.File;
 
 public class OsShareModule extends ReactContextBaseJavaModule {
 
@@ -20,8 +26,23 @@ public class OsShareModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    public void share(ReadableMap options, Promise promise) {
+        if (!options.hasKey("url")) {
+            Error e = new Error("Must provide url");
+            promise.reject("No URL", e);
+            return;
+        }
+
+        String url = options.getString("url");
+
+        Intent intentShareFile = new Intent(Intent.ACTION_SEND);
+        File fileWithinMyDir = new File(url);
+
+        if(fileWithinMyDir.exists()) {
+            intentShareFile.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://"+url));
+            intentShareFile.putExtra(Intent.EXTRA_SUBJECT, "Sharing File...");
+            intentShareFile.putExtra(Intent.EXTRA_TEXT, "Sharing File...");
+            this.reactContext.startActivity(Intent.createChooser(intentShareFile, "Share"));
+        }
     }
 }
