a5f88d2a6217b1225bcc5ee8038a7957cb988ae1
Desmond McNamee <desmondmcnamee@gmail.com>
2020-01-08 09:26:43 +0100

    Using main queue

--- ios/OsShare.m
+++ ios/OsShare.m
@@ -3,6 +3,15 @@
 
 @implementation OsShare
 
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+
++ (BOOL)requiresMainQueueSetup
+{
+    return YES;
+}
 
 RCT_EXPORT_MODULE()
 
